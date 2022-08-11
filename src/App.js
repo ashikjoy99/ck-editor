50c04339528fddf5fc69620cdc675dbe3a498e0b
ashikjoy <ashikjoy99@gmail.com>
2022-08-11 11:45:54 +0530

    ck editor

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import Editor from "ckeditor5-custom-build/build/ckeditor";
+// import {Editor as ClassicEditor} from 'ckeditor5-custom-build/build/ckeditor';
+import { CKEditor } from "@ckeditor/ckeditor5-react";
 
-function App() {
+const App = () => {
+  const [data, setData] = React.useState(
+    "<p>Banjos</p><img src='https://banjoslivestorage-ordering.s3.ap-southeast-2.amazonaws.com/home-banner/oa0JAlJwN5iYT6JvmpyaufsVL0z38Sjjompg05n8.png' />"
+  );
+  console.log("data", data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="App">
+        <h2>Using CKEditor 5</h2>
+        <CKEditor
+          editor={Editor}
+          data={data}
+          onReady={(editor) => {}}
+          onChange={(event, editor) => {
+            const data = editor.getData();
+            console.log({ event, editor, data });
+            setData(data);
+          }}
+          onBlur={(event, editor) => {}}
+          onFocus={(event, editor) => {}}
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
