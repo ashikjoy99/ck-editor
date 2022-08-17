87e932a4cb652adaa00ce72cad5488aba2a96b02
ashikjoy <ashikjoy99@gmail.com>
2022-08-17 14:12:18 +0530

    css styles

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ import { Route, Routes } from "react-router-dom";
 import Preview from "./Preview";
 
 const App = () => {
-  const [data, setData] = React.useState("<h1>Ab</h1><p>Helloo type here</p>");
+  const [data, setData] = React.useState(
+    '<h1>About Page</h1><p>&nbsp;</p><p style="margin-left:40px;">Banjo’s is a successful Bakery Café franchise with over 40 stores currently across Tasmania, Victoria, New South Wales, Queensland and South Australia, with more to open soon. Our delicious products are made by bakers&nbsp;</p><p style="margin-left:40px;"><img class="image_resized" style="width:30.79%;" src="https://banjosbeta.codinghands.in/storage/home-banner/dmwPrIY1ZUKtLo6JoPjzWaZ1ngp7leaCd0fUnRyF.jpg"></p><p style="margin-left:40px;">who can</p><p style="margin-left:40px;">re about using the best ingredients. Our simple philosophy results in a large number of loyal customers,</p><p style="margin-left:40px;">&nbsp;</p><p style="margin-left:40px;">&nbsp;many who make a visit to Banjo’s part of their daily routine.<br>Our success is not just for our benefit. &nbsp;We have a strong community focus and are committed to give back what we can in the form of sp</p><hr><p><br>&nbsp;</p>'
+  );
   const [isInitial, setIsInitial] = React.useState(true);
 
   useEffect(() => {
--- src/Preview.js
+++ src/Preview.js
@@ -2,7 +2,7 @@ import React from "react";
 import parse from "html-react-parser";
 
 const Preview = ({ data }) => {
-  return <div className="container">{parse(data)}</div>;
+  return <div className="container ck-content">{parse(data)}</div>;
 };
 
 export default Preview;
